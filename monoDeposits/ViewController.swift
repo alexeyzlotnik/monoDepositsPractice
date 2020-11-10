a2c5a114319c0ea8a03e79b91e95ca9443a1e162
alexeyzlotnik <alexeyzlotnik@gmail.com>
2020-11-10 13:37:08 +0200

    Logic for one of three buttons (uah, dollar, euro) to be selected

--- monoDeposits/ViewController.swift
+++ monoDeposits/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var uahButton: UIButton!{
+        didSet{
+            uahButton.isSelected = true
+        }
+    }
+    @IBOutlet weak var dollarButton: UIButton!
+    @IBOutlet weak var euroButton: UIButton!
     @IBOutlet weak var percentLabel: UILabel!
-    @IBOutlet weak var monthStepper: UIStepper!
+    @IBOutlet weak var monthStepper: UIStepper!{
+        didSet{
+            monthStepper.value = 12
+        }
+    }
     @IBOutlet weak var stepperMonthLabel: UILabel!
     @IBOutlet weak var stepperPercentLabel: UILabel!
     @IBOutlet weak var closeDepositAnyTimeSwitcher: UISwitch!
@@ -20,22 +31,7 @@ class ViewController: UIViewController {
     
     // создать енам с длительностю депозита и соответствующими ставками
     // сначала для гривны: чтобы отточить функционал, потом для доллара и евро
-    
-//    enum uahMonthes: Double, CaseIterable {
-//        case one = 1
-//        case two
-//        case three
-//        case four
-//        case five
-//        case six
-//        case seven = 7
-//        case eight = 10
-//        case nine = 10.5
-//        case ten = 10.5
-//        case eleven = 10.5
-//        case twelve = 11
-//    }
-//
+
     let uahMonthes = [
         "1": 8,
         "2": 8,
@@ -51,21 +47,50 @@ class ViewController: UIViewController {
         "12": 11
     ]
     
-//    var uahMonthes = Array(1...12)
+    let dollarMonthes = [
+        "1": 1,
+        "2": 1,
+        "3": 1.25,
+        "4": 1.25,
+        "5": 1.25,
+        "6": 1.5,
+        "7": 1.5,
+        "8": 1.5,
+        "9": 1.75,
+        "10": 1.75,
+        "11": 1.75,
+        "12": 2
+    ]
+    
+    let euroMonthes = [
+        "1": 1,
+        "2": 1,
+        "3": 1.25,
+        "4": 1.25,
+        "5": 1.25,
+        "6": 1.5,
+        "7": 1.5,
+        "8": 1.5,
+        "9": 1.75,
+        "10": 1.75,
+        "11": 1.75,
+        "12": 2
+    ]
+    
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+//        uahButton.setTitle("Гривна ✔", for: .selected)
+//        dollarButton.setTitle("Доллар ✔", for: .selected)
+//        euroButton.setTitle("Евро ✔", for: .selected)
         
     }
     
     @IBAction func monthStepperValueChanged(_ sender: UIStepper) {
-        // добавить или отнять месяц
-        // пробежаться по енаму
-        // менять значение percentLabel, stepperMonthLabel и stepperPercentLabel
+        
         let monthes = Int(monthStepper.value)
 
         stepperMonthLabel.text = "\(monthes) месяцев"
@@ -79,9 +104,49 @@ class ViewController: UIViewController {
     }
     
     @IBAction func closeDepositAnyTimeSwitcherValueChanged(_ sender: UISwitch) {
-        // прописать методы для isOn true/false
+        
+        if closeDepositAnyTimeSwitcher.isOn{
+            print("c досрочным расторжением")
+        }
     }
     
 
+    // Logic for one of three buttons to be selected
+
+    @IBAction func uahButtonTapped(_ sender: UIButton) {
+        if uahButton.isSelected {
+            // set deselected
+            uahButton.isSelected = false
+        } else {
+            // set selected
+            uahButton.isSelected = true
+            dollarButton.isSelected = false
+            euroButton.isSelected = false
+        }
+    }
+    
+    @IBAction func dollarButtonTapped(_ sender: UIButton) {
+        if dollarButton.isSelected {
+            // set deselected
+            dollarButton.isSelected = false
+        } else {
+            // set selected
+            dollarButton.isSelected = true
+            uahButton.isSelected = false
+            euroButton.isSelected = false
+        }
+    }
+    @IBAction func euroButtonTapped(_ sender: UIButton) {
+        if euroButton.isSelected {
+            // set deselected
+            euroButton.isSelected = false
+        } else {
+            // set selected
+            euroButton.isSelected = true
+            uahButton.isSelected = false
+            dollarButton.isSelected = false
+        }
+    }
+    
 }
 
